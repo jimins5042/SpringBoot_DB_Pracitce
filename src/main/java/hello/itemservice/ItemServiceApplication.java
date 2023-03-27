b3201e6151fbc43e88c62d3d9928b7a7f7fed5ca
timetable5678 <coolcats6762@gmail.com>
2023-03-28 00:12:29 +0900

    db_practice

---
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -0,0 +1,27 @@
+package hello.itemservice;
+
+import hello.itemservice.config.*;
+import hello.itemservice.repository.ItemRepository;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+
+//@Import(JdbcTemplateV3Config.class)	//메모리 컨픽을 임포트
+@Import(MyBatisConfig.class)	//메모리 컨픽을 임포트
+@SpringBootApplication(scanBasePackages = "hello.itemservice.web")
+public class ItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemServiceApplication.class, args);
+	}
+
+	@Bean
+	@Profile("local")	//운영환경에 따라 설정정보를 변환하기 위해 등록한 프로필
+	public TestDataInit testDataInit(ItemRepository itemRepository) {
+		return new TestDataInit(itemRepository);
+	}
+
+}
